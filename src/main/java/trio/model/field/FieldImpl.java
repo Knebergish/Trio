94927bf2a0722c32f3457f2ebff664f84e2ffa82
Temon137 <ak973@yandex.ru>
2019-03-26 20:54:50 +0400

    Исправлено копирование ячеек поля.

--- src/main/java/trio/model/field/FieldImpl.java
+++ src/main/java/trio/model/field/FieldImpl.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 
 public class FieldImpl implements Field, Serializable {
@@ -14,11 +15,15 @@ public class FieldImpl implements Field, Serializable {
 	
 	@JsonCreator
 	public FieldImpl(@JsonProperty("cells") CellType[][] cells) {
-		this.cells = cells.clone();
+		this.cells = cells;
 	}
 	
 	public CellType[][] copyCells() {
-		return cells.clone();
+		CellType[][] newCells = new CellType[cells.length][];
+		for (int i = 0; i < newCells.length; i++) {
+			newCells[i] = Arrays.copyOf(cells[i], cells[i].length);
+		}
+		return newCells;
 	}
 	
 	@Override
